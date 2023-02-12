fccc0561ef903b1ce86ed962b386805b7b0b5ae1
szjozi <szjozefczuk.sda@gmail.com>
2023-02-12 10:05:47 +0100

    Pre-work

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,9 +1,14 @@
 package com.sda;
 
+import com.sda.db.HibernateUtils;
+import org.hibernate.Session;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Session session = HibernateUtils.openSession();
+        session.close();
     }
 
 }
---
+++ src/test/java/com/sda/dao/UsersDAOTest.java
@@ -0,0 +1,7 @@
+package com.sda.dao;
+
+public class UsersDAOTest {
+
+    private final UsersDAO usersDAO = new UsersDAO();
+
+}
